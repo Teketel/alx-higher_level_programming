6eea197decb11a6821885ef4addcdbbc766ee148
teketel <for.teketel@gmail.com>
2022-10-17 12:34:29 +0300

    Done All tasks of  0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    l_length = len(my_list)
-    if idx < 0 or idx > l_length - 1:
-        return None
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+    if (idx < 0) or (idx > len(my_list) - 1):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
