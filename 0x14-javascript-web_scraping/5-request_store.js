84ba63828fab4b9490a31c26d9b788f0ada61eca
teketel <for.teketel@gmail.com>
2023-03-08 11:53:35 +0300

    Fix on 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const fileStream = fs.createWriteStream(process.argv[3]);
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
-});
+request
+  .get(url)
+  .pipe(fileStream);
